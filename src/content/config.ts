66ac8218e33b87d89e21f03930227f7af7b9d641
yudai hiyama <yudai1204h@gmail.com>
2024-06-17 00:40:20 +0900

    画像をsrcディレクトリ内に移行し、基本的にImageコンポーネントを使用するようにした

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,15 +3,18 @@ import { defineCollection, z } from "astro:content";
 const reports = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.string(),
-    updatedDate: z.string().optional(),
-    heroImage: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.string(),
+      updatedDate: z.string().optional(),
+      heroImage: image().refine((img) => img.width >= 240, {
+        message: "Hero image must be at least 240px wide",
+      }),
+      tags: z.array(z.string()).optional(),
+    }),
 });
 
 export const collections = { reports };
